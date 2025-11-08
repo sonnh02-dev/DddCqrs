a8d5e944c7e74d8b268664737d18d5c50cb1efdd
HE163733-Nguyen Hong Son <sonnh02.dev@gmail.com>
2025-11-09 00:08:35 +0700

    -Add GitHub Actions workflow for build and test -Refactor(infrastructure): update PublishDomainEventsInterceptor to improve domain event handling

---
+++ src/DddCqrs.Infrastructure/Data/PublishDomainEventsInterceptor.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using DddCqrs.SharedKernel;
+
+namespace DddCqrs.Infrastructure.Data;
+
+internal sealed class PublishDomainEventsInterceptor : SaveChangesInterceptor
+{
+    private readonly IPublisher _publisher;
+
+    public PublishDomainEventsInterceptor(IPublisher publisher)
+    {
+        _publisher = publisher;
+    }
+
+    public override async ValueTask<int> SavedChangesAsync(
+        SaveChangesCompletedEventData eventData,
+        int result,
+        CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is not null)
+        {
+            await PublishDomainEventsAsync(eventData.Context,cancellationToken);
+        }
+
+        return await base.SavedChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private async Task PublishDomainEventsAsync(DbContext context,CancellationToken cancellationToken)
+    {
+        var domainEvents = context
+            .ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                List<IDomainEvent> domainEvents = entity.DomainEvents;
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (IDomainEvent domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent,cancellationToken);
+        }
+    }
+}
